d23b253092b0ee2536b065d63a2e01b3de26360a
samo <msamotyj@gmail.com>
2024-10-23 18:06:56 +0200

    Drobne zmiany styli

--- public/js/pdfList.js
+++ public/js/pdfList.js
@@ -21,7 +21,7 @@ async function refreshPdfList() {
                         <a href="${pdf.url}" target="_blank" class="text-green-500 hover:text-green-700 mr-4 flex items-center" title="Pokaż">
                             <i class="fas fa-eye mr-1"></i> 
                         </a>
-                        <a href="${pdf.url}" download class="text-blue-500 hover:text-blue-700 flex items-center" title="Pobierz">
+                        <a href="${pdf.url}" class="text-blue-500 hover:text-blue-700 flex items-center" title="Pobierz">
                             <i class="fas fa-download mr-1"></i>
                         </a>
                     </div>
